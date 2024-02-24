71110cfa64eafdce70f427e9d35d844a4ce95517
alxdrr <lexlex277@gmail.com>
2024-02-24 21:20:01 +0700

    fix hero responsive

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,11 +3,11 @@ import heroImage from "../assets/Image.svg";
 import Light from "./light";
 function Navbar() {
   return (
-    <Flex height="100%" w="100%" maxW="1440" gap={16} align="center" justify="start" px={{ base: 6, md: 6, lg: 10 }} py={5}>
+    <Flex height="100%" w="100%" maxW="1440" align="center" px={{ base: 6, md: 6, lg: 10 }}>
       {/* <Light></Light> */}
-      <Flex align="center" justify="space-around" flexDir="row">
-        <Flex flexDir="column" maxW={{ base: "100%", md: "50%" }} gap={{ md: 6, xl: 6 }}>
-          <Flex flexDir="column">
+      <Flex h="100%" w="100%" align="center" justify="space-around" flexDir={{ base: "column-reverse", md: "row" }}>
+        <Flex flexDir="column" maxW={{ base: "100%", md: "50%" }} gap={{ base: 6, xl: 10 }} align={{ base: "center", md: "start" }}>
+          <Flex w="100%" flexDir="column" align={{ base: "center", md: "start" }}>
             <Text color="white" bgGradient="linear(to-l, #FFFFFF, #AEAEAE)" bgClip="text" fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }} fontWeight="bold">
               Charles Alexander
             </Text>
@@ -15,14 +15,9 @@ function Navbar() {
               UI/UX Designer
             </Text>
           </Flex>
-          <Text textStyle="p" color="white">
-            A digital jack-of-all-trades if you will. On one hand, I&apos;m a passionate UI/UX Designer. On the other, I&apos;m a versatile Fullstack Developer, coding and debugging my way to bring sleek. And if that wasn&apos;t enough,
-            I&apos;ve got a curious mind that&apos;s got me diving headfirst into the world of Machine Learning. I&apos;m excited by the thought of blending my design, development skills with AI to create unique digital solutions
-          </Text>
-          <Button>Hai</Button>
+          <Button size={{ base: "base", md: "md", lg: "lg" }}>Dive In</Button>
         </Flex>
-
-        <Box width={{ md: 240, lg: 320, xl: 360 }} display={{ base: "none", md: "inline" }}>
+        <Box width={{ base: 160, md: 240, lg: 320, xl: 360 }}>
           <Image h="100%" objectFit="fit" src={heroImage} alt="Profile Picture" />
         </Box>
       </Flex>
--- src/main.jsx
+++ src/main.jsx
@@ -11,18 +11,24 @@ const Button = defineStyleConfig({
     fontWeight: "semibold",
     borderRadius: "base", // <-- border radius is same for all variants and sizes
     border: "1px solid",
+    width: "fit-content",
   },
   // Two sizes: sm and md
   sizes: {
-    sm: {
-      fontSize: "sm",
-      px: 4, // <-- px is short for paddingLeft and paddingRight
-      py: 3, // <-- py is short for paddingTop and paddingBottom
+    base: {
+      fontSize: "base",
+      px: 6, // <-- px is short for paddingLeft and paddingRight
+      py: 2, // <-- py is short for paddingTop and paddingBottom
     },
     md: {
       fontSize: "md",
-      px: 6, // <-- these values are tokens from the design system
-      py: 4, // <-- these values are tokens from the design system
+      px: 8, // <-- these values are tokens from the design system
+      py: 2, // <-- these values are tokens from the design system
+    },
+    lg: {
+      fontSize: "lg",
+      px: 8, // <-- these values are tokens from the design system
+      py: 2, // <-- these values are tokens from the design system
     },
   },
   // Two variants: outline and solid
